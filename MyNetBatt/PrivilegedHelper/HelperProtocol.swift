b8e4a36d17bd837c680c7d36e85533f5d4086f1a
stone5202 <76396275+stone5202@users.noreply.github.com>
2026-09-14 20:11:06 +0800

    Use canonical MyNetBatt bundle identifier in helper XPC requirement

--- MyNetBatt/PrivilegedHelper/HelperProtocol.swift
+++ MyNetBatt/PrivilegedHelper/HelperProtocol.swift
@@ -7,5 +7,5 @@ import Foundation
 
 enum PrivilegedHelperConstants {
     static let machServiceName = "com.stone5202.MyNetBatt.PrivilegedHelper"
-    static let mainAppSigningRequirement = "anchor apple generic and identifier \"-23.MyNetBatt\""
+    static let mainAppSigningRequirement = "anchor apple generic and identifier \"com.stone5202.MyNetBatt\""
 }
