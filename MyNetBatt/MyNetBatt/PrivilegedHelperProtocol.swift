769efacaada3376957808eca4e91708c9503e37e
stone5202 <76396275+stone5202@users.noreply.github.com>
2026-09-14 20:11:00 +0800

    Use canonical MyNetBatt bundle identifier in app XPC requirements

--- MyNetBatt/MyNetBatt/PrivilegedHelperProtocol.swift
+++ MyNetBatt/MyNetBatt/PrivilegedHelperProtocol.swift
@@ -9,8 +9,8 @@ enum PrivilegedHelperConstants {
     static let machServiceName = "com.stone5202.MyNetBatt.PrivilegedHelper"
     static let daemonPlistName = "com.stone5202.MyNetBatt.PrivilegedHelper.plist"
 
-    // Xcode 目前專案的主 App signing identifier / bundle identifier。
-    // 如果你之後修改 PRODUCT_BUNDLE_IDENTIFIER，請同步修改 Helper 端的 requirement。
-    static let appSigningRequirement = "anchor apple generic and identifier \"-23.MyNetBatt\""
+    // 主 App 與 Helper 的 signing identifier / bundle identifier。
+    // 若日後修改 PRODUCT_BUNDLE_IDENTIFIER，兩端的 requirement 必須一起更新。
+    static let appSigningRequirement = "anchor apple generic and identifier \"com.stone5202.MyNetBatt\""
     static let helperSigningRequirement = "anchor apple generic and identifier \"com.stone5202.MyNetBatt.PrivilegedHelper\""
 }
