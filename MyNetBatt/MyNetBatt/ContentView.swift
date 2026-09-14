58450a7010654bcf8fd6afba081ad966f8389e9c
stone5202 <76396275+stone5202@users.noreply.github.com>
2026-09-14 13:18:35 +0800

    Add files via upload

---
+++ MyNetBatt/MyNetBatt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyNetBatt
+//
+//  Created by 沈昱辰 on 2026/9/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
